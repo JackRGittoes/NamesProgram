2d2594c7f601d3a70ff3a3c5709ed4f03dcde770
Jack Gittoes <Jrgittoes@gmail.com>
2020-06-17 15:35:47 +0100

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PersonProgram
+{
+    public class Person
+    {
+        string Name;
+
+
+        public void Introduce()
+        {
+
+            Console.WriteLine("Your Name Is {0}", Name);
+            Console.ReadLine();
+        }
+        public string NewName(string name)
+        {
+            this.Name = name;
+            return name;
+        }
+            
+
+            
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            var input = 1;
+            for (int i = 0; i < input;)
+            {
+                var person = new Person();
+                Console.WriteLine("Input a name");
+                var name = Console.ReadLine();
+                person.NewName(name);
+                person.Introduce(); 
+                names.Add(name);
+                
+
+                Console.WriteLine("Would you like to add another name? \n [1] Yes \n [2] No");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 2)
+                {
+                    input = 0;
+                    
+                }
+            }
+            Console.WriteLine("The names you have provided are");
+            
+            for(int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
